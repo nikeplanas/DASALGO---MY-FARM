0e9ef717b0d7b3bdf21f7ebcdadca6bc8c3cf333
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-09 09:41:16 +0800

    Update Flower.java

--- MyFarm/Model/Flower.java
+++ MyFarm/Model/Flower.java
@@ -1,24 +1,8 @@
 package MyFarm.Model;
 
 public class Flower extends Seed {
-    private String name;
-    private int hello;
 
-    @Override
-    public String getName() {
-        return name;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHello() {
-        return hello;
-    }
-
-    public void setHello(int hello) {
-        this.hello = hello;
+    public Flower () {
+        super();
     }
 }
