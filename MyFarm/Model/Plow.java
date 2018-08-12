2431fa5e59e9f6918a0a6fe8d1f899854f6d9fd0
shashashabaw <39220915+shashashabaw@users.noreply.github.com>
2018-08-12 14:37:32 +0800

    Update Plow.java

--- MyFarm/Model/Plow.java
+++ MyFarm/Model/Plow.java
@@ -6,4 +6,8 @@ public class Plow extends Item {
         this.setName("Plow");
         this.setDescription("Used to plow tiles and remove withered plants.");
     }
+    
+    public void plowTile (Tile t) {
+        t.setPlowed();
+    }
 }
