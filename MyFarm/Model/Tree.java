6f5708c15f2ec8a484c55ee91ad7d61413bc054d
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 15:35:59 +0800

    Update Tree.java

--- MyFarm/Model/Tree.java
+++ MyFarm/Model/Tree.java
@@ -1,4 +1,17 @@
 package MyFarm.Model;
 
+/**	This class is a representation of the Tree model.
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
+
 public class Tree extends Seed {
+
+	/**
+	 * This constructor instantiates Tree
+	 */
+	public Tree () {
+		Super();
+	}
 }
