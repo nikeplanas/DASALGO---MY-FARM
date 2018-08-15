e74bf09badf9db64cded08fd511dd1d96f9a5809
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 22:18:59 +0800

    Update Item.java

--- MyFarm/Model/Item.java
+++ MyFarm/Model/Item.java
@@ -1,26 +1,51 @@
 package MyFarm.Model;
 
+/**	This abstract class is a representation of the Item model.
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
+
 public abstract class Item {
-    private String name;
-    private String description;
+	private String name;
+	private String description;
 
-    public Item () {
+	/**
+	 * This constructor instantiates Item
+	 */
+	public Item () {
 
-    }
+	}
 
-    public String getDescription () {
-        return description;
-    }
+	/**
+	 * This method is the getter for description
+	 * @return the description of the Item
+	 */
+	public String getDescription () {
+		return description;
+	}
 
-    public String getName () {
-        return name;
-    }
+	/**
+	 * This method is the getter for name
+	 * @return the name of the Item
+	 */
+	public String getName () {
+		return name;
+	}
 
-    public void setName (String name) {
-        this.name = name;
-    }
+	/**
+	 * This method is the setter for name
+	 * @param name where the name is set
+	 */
+	public void setName (String name) {
+		this.name = name;
+	}
 
-    public void setDescription (String description) {
-        this.description = description;
-    }
+	/**
+	 * This method is the setter for description
+	 * @param description where the description is set
+	 */
+	public void setDescription (String description) {
+		this.description = description;
+	}
 }
