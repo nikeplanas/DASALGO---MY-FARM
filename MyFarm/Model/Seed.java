1c2f3e9286b6951629940ee400a67ef87e226098
shashashabaw <39220915+shashashabaw@users.noreply.github.com>
2018-08-15 15:31:27 +0800

    Update Seed.java

--- MyFarm/Model/Seed.java
+++ MyFarm/Model/Seed.java
@@ -1,14 +1,17 @@
 package MyFarm.Model;
-import.java.util.Random;
 
+import java.util.Random;
+
+/**	This abstract class is a representation of a seed in MyFarm
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
 public abstract class Seed extends Item {
-    private String name;
     private long harvestTime;
-    private long secondsNeeded;
-    private int timesWatered;
+    private long harvestTimeBonus;
     private int waterNeeded;
     private int waterLimit;
-    private int timesFertilized;
     private int fertilizerNeeded;
     private int fertilizerLimit;
     private int harvestCost;
@@ -16,6 +19,13 @@ public abstract class Seed extends Item {
     private double seedCost;
     private double baseSelling;
     private int amount;
+
+    /** This is used to construct the class. The harvestTimeBonus is set to 0.
+     *  The rest of the attributes will be set using setters.
+     */
+    public Seed () {
+        harvestTimeBonus = 0;
+    }
 
 
     @Override
@@ -31,16 +41,18 @@ public abstract class Seed extends Item {
                 "\tPossible No. of Products: " + productsProduced;
     }
 
+    /** This method overrides the toString() method which is used for checking the values
+     *  of the attributes of a Seed object
+     *
+     * @return a string containing the attributes
+     */
     @Override
     public String toString() {
         return "Seed{" +
                 "name=" + getName() +
                 "harvestTime=" + harvestTime +
-                ", secondsNeeded=" + secondsNeeded +
-                ", timesWatered=" + timesWatered +
                 ", waterNeeded=" + waterNeeded +
                 ", waterLimit=" + waterLimit +
-                ", timesFertilized=" + timesFertilized +
                 ", fertilizerNeeded=" + fertilizerNeeded +
                 ", fertilizerLimit=" + fertilizerLimit +
                 ", harvestCost=" + harvestCost +
@@ -51,61 +63,109 @@ public abstract class Seed extends Item {
                 '}';
     }
 
+    /** It subtracts the harvest time bonus from the harvest time of the Seed
+     *  and returns it.
+     *
+     * @return The number of seconds until the plant is ready for harvest.
+     */
+
+    public long getHarvestTimeWithBonus() {
+        return harvestTime - harvestTimeBonus;
+    }
+
+    /** Gets the value of harvest time.
+     *
+     * @return The base time it takes to harvest the plant.
+     */
+
     public long getHarvestTime() {
         return harvestTime;
     }
+
+    /** Sets the value for harvest time.
+     *
+     * @param harvestTime The base time it takes to harvest the plant.
+     */
 
     public void setHarvestTime(long harvestTime) {
         this.harvestTime = harvestTime;
     }
 
-    public long getSecondsNeeded() {
-        return secondsNeeded;
-    }
-
-    public void setSecondsNeeded(long secondsNeeded) {
-        this.secondsNeeded = secondsNeeded;
-    }
-
-    public int getTimesWatered() {
-        return timesWatered;
-    }
-
-    public void setTimesWatered(int timesWatered) {
-        this.timesWatered = timesWatered;
-    }
+    /** Gets the value of the waterNeeded attribute.
+     *
+     * @return The minimum number of times needed to water the plant for it to survive.
+     */
 
     public int getWaterNeeded() {
         return waterNeeded;
     }
 
+    /** Sets the value of waterNeeded
+     *
+     * @param waterNeeded The value to be saved in waterNeeded.
+     */
     public void setWaterNeeded(int waterNeeded) {
         this.waterNeeded = waterNeeded;
     }
+
+    /** Gets the value of the bonus to harvest time from ranking up.
+     *
+     * @return The bonus time to be subtracted from the base harvest time.
+     */
+
+    public long getHarvestTimeBonus() {
+        return harvestTimeBonus;
+    }
+
+    /** Sets the value for the bonus to harvest time
+     *
+     * @param harvestTimeBonus
+     */
+
+    public void setHarvestTimeBonus(long harvestTimeBonus) {
+        this.harvestTimeBonus = harvestTimeBonus;
+    }
+
+    /** Gets the maximum number of times a plant can be watered.
+     *
+     * @return The maximum number of times a plant can be watered.
+     */
 
     public int getWaterLimit() {
         return waterLimit;
     }
 
+    /** Sets the value for waterLimit
+     *
+     * @param waterLimit the value to be saved to waterLimit.
+     */
     public void setWaterLimit(int waterLimit) {
         this.waterLimit = waterLimit;
     }
 
-    public int getTimesFertilized() {
-        return timesFertilized;
-    }
-
-    public void setTimesFertilized(int timesFertilized) {
-        this.timesFertilized = timesFertilized;
-    }
+    /** Gets the minimum fertilizer needed for a plant to survive.
+     *
+     * @return The attribute fertilizerNeeded.
+     */
 
     public int getFertilizerNeeded() {
         return fertilizerNeeded;
     }
 
+    /** Sets the value for fertilizerNeeded
+     *
+     * @param fertilizerNeeded The value to be saved to fertilizerNeeded.
+     */
+
     public void setFertilizerNeeded(int fertilizerNeeded) {
         this.fertilizerNeeded = fertilizerNeeded;
     }
+
+    /** Gets the limit to the times a soil can be fertilized and for it to apply
+     *  to the plant.
+     *
+     * @return Maximum value to be applied in computing the fb(fertilizer bonus)
+     */
 
     public int getFertilizerLimit() {
         return fertilizerLimit;
@@ -155,15 +215,6 @@ public abstract class Seed extends Item {
         this.amount = amount;
     }
 
-    public double buy (double myFarmCoins) {
-        return myFarmCoins;
-    }
-
-    public void plant (Tile t) {
-
-        t.setSeed;
-    }
-    
     public int convertPPtoRandomIntInRange () {
         Random rand = new Random();
         if (productsProduced.contains("-")) {
@@ -175,17 +226,9 @@ public abstract class Seed extends Item {
         }
         return Integer.parseInt(productsProduced.trim());
     }
-    
-    /*
 
-    public double computeSP (Tile t) {
-        double sp;
-
-        return sp;
-    }
-    */
-    public void updateBonusLimits () {
-
+    public void addAmount() {
+        amount++;
     }
 
 
