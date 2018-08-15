e94aa8b54a84cd3a07356d12a83740df217ad638
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 15:31:51 +0800

    Update Plow.java

--- MyFarm/Model/Plow.java
+++ MyFarm/Model/Plow.java
@@ -1,13 +1,27 @@
 package MyFarm.Model;
 
+/**	This class is a representation of the Plow model.	 
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
+
 public class Plow extends Item {
 
-    public Plow () {
-        this.setName("Plow");
-        this.setDescription("Used to plow tiles and remove withered plants.");
-    }
-    
-    public void plowTile (Tile t) {
-        t.setPlowed();
-    }
+	/**
+	 * This method sets name and description for Plow object
+	 */
+	public Plow () {
+		this.setName("Plow");
+		this.setDescription("Used to plow tiles and remove withered plants.");
+	}
+
+	/**
+	 * This method sets the current t to plowed
+	 * @param t is the current tile
+	 */
+
+	public void plowTile (Tile t) {
+		t.setPlowed();
+	}
 }
