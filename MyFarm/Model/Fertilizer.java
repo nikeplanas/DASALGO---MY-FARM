06348d7a289a290329a47cbc57f90987a3f61bdd
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 15:41:39 +0800

    Update Fertilizer.java

--- MyFarm/Model/Fertilizer.java
+++ MyFarm/Model/Fertilizer.java
@@ -7,14 +7,19 @@ package MyFarm.Model;
  */
 public class Fertilizer extends Item {
 	private int amount;
+	
+	/**
+	* This constructor instantiates Fertilizer which also sets the amount to 0 and sets the name and
+	* description for the respective object.
+	*/
 	public Fertilizer () {
 		amount = 0;
 		this.setName("Fertilizer");
 		this.setDescription("Not actually a tool, but is bought in finite amounts and fertilizes a specific tile; Cannot be placed on a tile with a plant.");
 	}
 
-	/**	This method is the getter for amount of fertilizer
-	 *
+	/**	
+	 *	This method is the getter for amount of fertilizer
 	 *	@return the amount of fertilizer
 	 *
 	 */
@@ -29,9 +34,9 @@ public class Fertilizer extends Item {
 		amount++;
 	}
 
-	/** This method adds fertilizer to the tile.
+	/** 
+	 * This method adds fertilizer to the tile.
 	 * Decreases amount of current fertilizer for every use.
-	 *
 	 * @param t the current Tile
 	 */
 	public void fertilizeTile (Tile t) {
