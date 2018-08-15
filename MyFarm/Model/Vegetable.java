2a9c3f85bdfaf215920104c58e19349e81712171
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 15:37:25 +0800

    Update Vegetable.java

--- MyFarm/Model/Vegetable.java
+++ MyFarm/Model/Vegetable.java
@@ -1,6 +1,16 @@
 package MyFarm.Model;
 
+/**	This class is a representation of the Vegetable model.
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
+
 public class Vegetable extends Seed {
+    
+    /**
+    * This constructor instantiates Vegetable
+    */
     public Vegetable () {
         Super();
     }
