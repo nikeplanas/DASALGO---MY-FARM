d52f571a77df8c9bcef9785be01fc52b5eb598c5
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 15:39:35 +0800

    Update WateringCan.java

--- MyFarm/Model/WateringCan.java
+++ MyFarm/Model/WateringCan.java
@@ -1,13 +1,26 @@
 package MyFarm.Model;
 
+/**	This class is a representation of the WateringCan model.
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
+
 public class WateringCan extends Item {
 
-    public WateringCan () {
-        this.setName("Watering Can");
-        this.setDescription("Used to water tiles.");
-    }
-    
-    public void waterTile (Tile tile) {
-        tile.addWater();
-    }
+	/**
+	 * This constructor instantiates WateringCan and sets the name and description for the respective object
+	 */
+	public WateringCan () {
+		this.setName("Watering Can");
+		this.setDescription("Used to water tiles.");
+	}
+
+	/**
+	 * This method adds water to the current tile
+	 * @param tile current tile on to add water
+	 */
+	public void waterTile (Tile tile) {
+		tile.addWater();
+	}
 }
