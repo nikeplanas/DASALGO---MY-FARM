604b855d7e42454e66d34971ea8ce4fcdc8b153c
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 22:11:35 +0800

    Update Fruit.java

--- MyFarm/Model/Fruit.java
+++ MyFarm/Model/Fruit.java
@@ -1,8 +1,17 @@
 Package MyFarm.Model;
 
+
+/**	This class is a representation of the flower model.
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
 public class Fruit {
 
+	/**
+	* This constructor instantiates Fruit
+	*/
     public Fruit () {
-	super();
+	Super();
     }
 }
