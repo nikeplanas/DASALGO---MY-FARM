ed3a05d6bb467ca865b99837d56da3f1d4eb2a05
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 15:24:03 +0800

    Update Fertilizer.java

--- MyFarm/Model/Fertilizer.java
+++ MyFarm/Model/Fertilizer.java
@@ -6,30 +6,38 @@ package MyFarm.Model;
  *	@version 1.0
  */
 public class Fertilizer extends Item {
-    private int amount;
-    public Fertilizer () {
-        amount = 0;
-        this.setName("Fertilizer");
-        this.setDescription("Not actually a tool, but is bought in finite amounts and fertilizes a specific tile; Cannot be placed on a tile with a plant.");
-    }
+	private int amount;
+	public Fertilizer () {
+		amount = 0;
+		this.setName("Fertilizer");
+		this.setDescription("Not actually a tool, but is bought in finite amounts and fertilizes a specific tile; Cannot be placed on a tile with a plant.");
+	}
 
-/**	This method is the getter for amount
- *	
- *	@return the 
- *	
- */
-    public int getAmount() {
-        return amount;
-    }
+	/**	This method is the getter for amount of fertilizer
+	 *
+	 *	@return the amount of fertilizer
+	 *
+	 */
+	public int getAmount() {
+		return amount;
+	}
 
-    public void addAmount() {
-        amount++;
-    }
-    
-    public void fertilizeTile (Tile t) {
-        if (t.getSeed() == null) {
-            t.addFertilizer();
-            amount--;
-        }
-    }
+	/**
+	 * This method counts the amount of fertilizer every increment.
+	 */
+	public void addAmount() {
+		amount++;
+	}
+
+	/** This method adds fertilizer to the tile.
+	 * Decreases amount of current fertilizer for every use.
+	 *
+	 * @param t the current Tile
+	 */
+	public void fertilizeTile (Tile t) {
+		if (t.getSeed() == null) {
+			t.addFertilizer();
+			amount--;
+		}
+	}
 }
