b2c8df9b291f1ef5ee5bfa2b483b6127b578aa0a
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 15:28:26 +0800

    Update Pickaxe.java

--- MyFarm/Model/Pickaxe.java
+++ MyFarm/Model/Pickaxe.java
@@ -1,15 +1,27 @@
 package MyFarm.Model;
 
+    /**	This class is a representation of the Pickaxe model.
+     *	@author Ma. Patricia Y. Villaroman
+     *	@author Nicole Danielle Planas
+    *	@version 1.0
+     */
 public class Pickaxe extends Item {
 
-    public Pickaxe () {
-        this.setName("Pickaxe");
-        this.setDescription("Used for destroying rocks obstructing tiles.");
-    }
+    /** This method sets the name and description of object Pickaxe
+    *
+    */
+	public Pickaxe () {
+		this.setName("Pickaxe");
+		this.setDescription("Used for destroying rocks obstructing tiles.");
+	}
 
-    public void breakRock(Tile t) {
-        if (t.isObstructed()) {
-            t.breakRock();
-        }
-    }
+	/** This method checks if the t is obstructed before breaking rock
+	 * 
+	 * @param t the current Tile
+	 */
+	public void breakRock(Tile t) {
+		if (t.isObstructed()) {
+			t.breakRock();
+		}
+	}
 }
