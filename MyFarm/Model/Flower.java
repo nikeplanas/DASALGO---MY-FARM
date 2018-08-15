c7c28206f74b2a31db68d85a9741a606d22850f1
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2018-08-15 22:10:38 +0800

    Update Flower.java

--- MyFarm/Model/Flower.java
+++ MyFarm/Model/Flower.java
@@ -1,8 +1,17 @@
 package MyFarm.Model;
 
+
+/**	This class is a representation of the flower model.
+ *	@author Ma. Patricia Y. Villaroman
+ *	@author Nicole Danielle Planas
+ *	@version 1.0
+ */
 public class Flower extends Seed {
 
+    /**
+    * This constructor instantiates flower
+    */
     public Flower () {
-        super();
+        Super();
     }
 }
